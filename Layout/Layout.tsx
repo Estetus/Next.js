7e719d84d8ee563c81ab1a9d2489065fe5ad83aa
artem <bazarow-artem@mail.ru>
2025-10-20 23:27:45 +0300

    add framer

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -2,14 +2,26 @@
 import { Header } from './Header/Header';
 import { type LayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
+import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
 
 export const Layout = ( {children}: LayoutProps)  => {
+  const router = useRouter();
+
 return (
   <div className={styles.container}>
-  <Header className={styles.header}/>
-  <div className={styles.body}>
-    {children}
-  </div>
+    <Header className={styles.header} />
+    <div className={styles.body}>
+      <motion.div
+      className={styles.card}
+        key={router.route}
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        {children}
+      </motion.div>
+    </div>
   </div>
 );
 };
