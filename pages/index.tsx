053a1ad20817001cf1ecaf8dc457663f91a08aad
Estetus <91020134+Estetus@users.noreply.github.com>
2025-10-23 21:17:57 +0300

    Revert "5 final"

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,48 +5,35 @@ import { type GetStaticProps } from 'next';
 import axios from 'axios';
 import { type Post } from '@/interfaces/posts.interface';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
 
 
+function Home({posts, gitUrl}: HomeProps): JSX.Element {
 
-function Home({ posts, gitUrl }: HomeProps): JSX.Element {
   return (
-    <>
-      {posts.map((p, index) => (
-        <motion.div
-          key={p.id}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 0.4,
-            delay: index * 0.1,
-          }}
-          whileHover={{ scale: 1.02 }}
-          whileTap={{ scale: 0.98 }}
-        >
-          <Link href={`/posts/${p.id}`}>
-            <Card size="s">
-              {p.title}
-              {p.body}
-            </Card>
-          </Link>
-        </motion.div>
-      ))}
+    <> 
+      { posts.map((p) => (
+        <Link key={p.id} href={`/posts/${p.id}`}>
+        <Card size='s'>
+          {p.title}
+          {p.body}
+        </Card>
+        </Link>
+      ))
+      }
     </>
   );
 }
 
+
 export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-  const { data: posts } = await axios.get<Post[]>(
-    `${process.env.NEXT_PUBLIC_DOMAIN}/posts`
-  );
-
+   const { data: posts } = await axios.get<Post[]>(`${process.env.NEXT_PUBLIC_DOMAIN}/posts`);
+    
   return {
     props: {
       posts: posts.slice(0, 10),
-      gitUrl: 'https://github.com/Estetus/',
+      gitUrl: 'https://github.com/Estetus/'
     },
   };
 };
